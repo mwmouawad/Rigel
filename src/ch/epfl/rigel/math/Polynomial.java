04dd1adb9770ca534b9d7618128fd5012d3e53ec
mwmouawad <mwmouawad@gmail.com>
2020-02-24 15:37:58 +0100

    ToString  of Polynomial Added. All tests Partie 1 passing.

--- src/ch/epfl/rigel/math/Polynomial.java
+++ src/ch/epfl/rigel/math/Polynomial.java
@@ -28,10 +28,17 @@ public final class Polynomial {
 
     public double at(double x){
 
+        //Take a look at the degree of the Polynomial
+        int coeffLength = this.coefficients.length;
+
+        if(coeffLength == 1){
+            return this.coefficients[0];
+        }
+
         double value = this.coefficients[0] * x + this.coefficients[1];
 
 
-        for(var i = 2; i< this.coefficients.length; i++){
+        for (var i = 2; i < this.coefficients.length; i++) {
 
             value = value * x + this.coefficients[i];
         }
@@ -41,10 +48,63 @@ public final class Polynomial {
     }
 
     @Override
+    //TODO
     public String toString() {
         StringBuilder sb = new StringBuilder();
 
+        //Check the degree of the Polynomial
+        int coeffLength = this.coefficients.length;
+
+        if(coeffLength == 1){
+            sb.append(this.coefficients[0]);
+        }
+        else if (coeffLength > 1){
+
+
+            for(var i = 0;i<coeffLength ; i++ ){
+
+                if(this.coefficients[i] != 0 ){
+
+                    if(this.coefficients[i] == 1){
+
+                    }
+                    else if(this.coefficients[i] == -1){
+                        sb.append("-");
+                    }
+                    else{
+                        sb.append(this.coefficients[i]);
+                    }
+
+                    //Add x to the string if it isn't the last coefficient
+                    if(i < coeffLength - 1){
+
+                        sb.append("x");
+                        //Variable storing the current loop degree x of the polynomial
+                        int currentDeg = coeffLength - i -1;
+                        //If degree equal x do not add the ^ and the degree coefficient.
+                        if(currentDeg != 1){
+                            sb.append("^");
+                            sb.append(currentDeg);
+                        }
+
+                        //Check if the next coefficient is negative. Otherwise add a "+" string.
+                        if(this.coefficients[i+1] > 0){
+                            sb.append("+");
+                        }
+
+                    }
+
+                }
+
+
+
+            }
+
+
+        }
+
         return sb.toString();
+
     }
 
     @Override
