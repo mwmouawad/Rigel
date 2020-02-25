9574d1bf392e2dcd2afdebf90463b7736cadf1d1
mwmouawad <mwmouawad@gmail.com>
2020-02-25 17:13:30 +0100

    Added Author to Spherical Coordinates test

--- test/ch/epfl/rigel/coordinates/SphericalCoordinatesTest.java
+++ test/ch/epfl/rigel/coordinates/SphericalCoordinatesTest.java
@@ -3,7 +3,10 @@ package ch.epfl.rigel.coordinates;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-
+/**
+ * @author Mark Mouawad (296508)
+ * @author Leah Uzzan (302829)
+ */
 public class SphericalCoordinatesTest {
 
 
