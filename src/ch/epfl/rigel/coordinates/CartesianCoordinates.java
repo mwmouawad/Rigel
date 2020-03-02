0895514c0aeb8c32391aeeef642c7dc75928891c
Leah Uzzan <leah.uzzan@epfl.ch>
2020-03-02 15:26:04 +0100

    step 4 to review, check and test

---
+++ src/ch/epfl/rigel/coordinates/CartesianCoordinates.java
@@ -0,0 +1,36 @@
+package ch.epfl.rigel.coordinates;
+
+import java.util.Locale;
+
+public final class CartesianCoordinates {
+
+    private double x;
+    private double y;
+
+    private CartesianCoordinates(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public static CartesianCoordinates of(double x, double y){
+        return new CartesianCoordinates(x,y);
+    }
+
+    public double x() { return x; }
+    public double y() { return y; }
+
+    @Override
+    public boolean equals(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int hashCode() {
+        throw new UnsupportedOperationException();
+    }
+
+    //TODO check if correct
+    public String toString() {
+        return String.format(Locale.ROOT, "(x= %.4f, y= %.4f)", x,
+                y); }
+}
---
+++ src/ch/epfl/rigel/coordinates/StereographicProjection.java
@@ -0,0 +1,77 @@
+package ch.epfl.rigel.coordinates;
+
+import java.util.Locale;
+import java.util.function.Function;
+
+public final class StereographicProjection implements Function<HorizontalCoordinates, CartesianCoordinates> {
+
+    private final double centerX;
+    private final double centerY;
+    private final double sinCenterY;
+    private final double cosCenterY;
+
+    public StereographicProjection(HorizontalCoordinates center) {
+        centerX = center.az();
+        centerY = center.alt();
+        sinCenterY = Math.sin(centerY);
+        cosCenterY = Math.cos(centerY);
+
+    }
+
+    @Override
+    public CartesianCoordinates apply(HorizontalCoordinates azAlt) {
+        double coordX = azAlt.az();
+        double coordY = azAlt.alt();
+        double varX = coordX - centerX;
+
+        double d = 1/(1 + Math.sin(coordY)* sinCenterY + Math.cos(coordY)*cosCenterY*Math.cos(varX));
+        //computes the new coordinates
+        double x = d*Math.cos(coordY)*Math.sin(varX);
+        double y = d*(Math.sin(coordY)*cosCenterY - Math.cos(coordY)*sinCenterY*Math.cos(varX));
+        return CartesianCoordinates.of(x,y);
+    }
+
+    public CartesianCoordinates circleCenterForParallel(HorizontalCoordinates hor){
+        double coordY = cosCenterY/(Math.sin(hor.alt()) + sinCenterY);
+        return CartesianCoordinates.of(0, coordY);
+    }
+
+    public double circleRadiusForParallel(HorizontalCoordinates parallel){
+        return (Math.cos(parallel.alt())/(Math.sin(parallel.alt()) + sinCenterY));
+    }
+
+
+    public double applyToAngle(double rad){
+        //TODO
+        return 0;
+    }
+
+    public HorizontalCoordinates inverseApply(CartesianCoordinates xy){
+        double rho = Math.sqrt(xy.x()*xy.x() + xy.y()*xy.y());
+        double sinc = 2*rho/(rho*rho + 1);
+        double cosc= (1-rho*rho)/(rho*rho + 1);
+
+        //computes inverse coordinates with the above constants
+        double lambda = Math.atan2(xy.x()*sinc, rho*cosCenterY*cosc - xy.y()*sinCenterY*sinc) + centerX;
+        double phi = Math.asin(cosc*sinCenterY + (xy.y()*sinc*cosCenterY)/rho);
+        return HorizontalCoordinates.of(lambda, phi);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int hashCode() {
+        throw new UnsupportedOperationException();
+    }
+
+    //TODO check if correct, only want center ?
+    public String toString() {
+        return String.format(Locale.ROOT, "StereographicProjection center : (x= %.4f, y= %.4f)", centerX,
+                centerY);
+    }
+
+
+}
