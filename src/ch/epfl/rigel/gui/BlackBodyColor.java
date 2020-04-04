37e11c98ffeb13deb7ac94797084a95fb2211e1b
Leah Uzzan <leah.uzzan@epfl.ch>
2020-04-04 18:18:29 +0200

    BlackBodyColor structure

--- src/ch/epfl/rigel/gui/BlackBodyColor.java
+++ src/ch/epfl/rigel/gui/BlackBodyColor.java
@@ -20,10 +20,10 @@ import javafx.scene.paint.Color;
 public class BlackBodyColor {
 
     private static final String BBR_COLOR = "/bbr_color.txt";
-    private static final Interval tempInterval = ClosedInterval.of(1000, 40000);
+    private static final Interval TEMP_INTERVAL = ClosedInterval.of(1000, 40000);
     private static final Map<Integer, String> colorMap = loadColors();
 
-    private BlackBodyColor() throws IOException {
+    private BlackBodyColor() {
     }
 
 
@@ -37,44 +37,32 @@ public class BlackBodyColor {
             BufferedReader buffReader = new BufferedReader(inStrReader);
 
             String line;
-            String[] charTable;
-            String colorCode = null;
 
             for (int i = 0; i < 18; i++) buffReader.readLine();
             line = buffReader.readLine();
             int lineNb = 19;
 
-
             while (line != null) {
 
-                System.out.println(lineNb);
                 if (lineNb > 19 && lineNb < 802) {
                     StringBuilder sb = new StringBuilder();
 
+                    //TODO : Pourquoi ne fonctionne pas? String sb = line.substring(0,6);
                     for (int i = 0; i < 6; i++) {
-                        if (line.charAt(i) != ' ')
-                            sb.append(line.charAt(i));
-
+                      sb = line.charAt(i) != ' ' ? sb.append(line.charAt(i)) : sb;
                     }
 
                     int temp = Integer.parseInt(sb.toString());
-                    sb = new StringBuilder();
+                    String colorCode = line.substring(line.indexOf("#"));
 
-                    for (int i = 79; i < 87; i++) {
-                        if (line.charAt(i) != ' ') {
-                            sb.append(line.charAt(i));
-                        }
-                    }
-                    colorCode = sb.toString();
                     map.put(temp, colorCode);
+                    System.out.println(lineNb + " " + temp  + " " + colorCode);
                 }
 
                 lineNb += 2;
                 buffReader.readLine();
                 line = buffReader.readLine();
-
             }
-
             return map;
 
         } catch (IOException e) {
@@ -91,9 +79,8 @@ public class BlackBodyColor {
      */
     public static Color colorForTemperature(double temperatureInDeg) {
 
-
         //Check if the temperature is in the right interval
-        Preconditions.checkInInterval(tempInterval, temperatureInDeg);
+        Preconditions.checkInInterval(TEMP_INTERVAL, temperatureInDeg);
 
         int roundedTemp = (int) Math.round(temperatureInDeg / 100.0d) * 100;
 
