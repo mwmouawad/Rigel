86a7ce661b33ff40906c4e537a819b97816f400a
Leah Uzzan <leah.uzzan@epfl.ch>
2020-04-23 08:16:26 +0200

    step 9 start

--- src/ch/epfl/rigel/gui/NamedTimeAccelerator.java
+++ src/ch/epfl/rigel/gui/NamedTimeAccelerator.java
@@ -1,7 +1,20 @@
 package ch.epfl.rigel.gui;
 
-public enum NamedTimeAccelerator {
-    ;
+import java.time.Duration;
 
-    private NamedTimeAccelerator(){}
+public enum NamedTimeAccelerator {
+    TIMES_1("1x", TimeAccelerator.continuous(1)),
+    TIMES_30("30x", TimeAccelerator.continuous(30)),
+    TIMES_300("300x", TimeAccelerator.continuous(300)),
+    TIMES_3000("3000x", TimeAccelerator.continuous(3000)),
+    DAY("jour", TimeAccelerator.discrete(Duration.ofHours(24), Duration.ofSeconds(1/10)));
+    //SIDEREAL_DAY("jour sidéral", TimeAccelerator.discrete(Duration.o, Duration.ofSeconds(1/10))),
+
+    String name;
+    TimeAccelerator accelerator;
+
+    NamedTimeAccelerator(String name, TimeAccelerator accelerator){
+        this.name = name;
+        this.accelerator = accelerator;
+    }
 }
--- src/ch/epfl/rigel/gui/TimeAccelerator.java
+++ src/ch/epfl/rigel/gui/TimeAccelerator.java
@@ -1,6 +1,7 @@
 package ch.epfl.rigel.gui;
 
 
+import java.time.Duration;
 import java.time.ZonedDateTime;
 
 @FunctionalInterface
@@ -8,4 +9,12 @@ public interface TimeAccelerator {
 
     // une valeur de type long exprimée en nanosecondes?
     ZonedDateTime adjust(ZonedDateTime initialTime, long time);
+
+    static TimeAccelerator continuous(double alpha){
+        return null;
+    }
+
+    static TimeAccelerator discrete(Duration step, Duration frequence){
+        return null;
+    }
 }
